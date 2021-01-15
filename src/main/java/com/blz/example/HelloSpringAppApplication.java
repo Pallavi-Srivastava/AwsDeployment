f39bfaac2c6f00983f21594f47c8783a5d9ef509
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2021-01-16 00:36:05 +0530

    Added:Hello SpringDemo Project

---
+++ src/main/java/com/blz/example/HelloSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.blz.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringAppApplication.class, args);
+	}
+
+}
